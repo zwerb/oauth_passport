cd7f4a5da3f23e430429cb3d35285fc303b9df22
Thomas Kelly <tmkelly28@gmail.com>
2018-07-18 14:51:21 -0400

    update start and remove body parser

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
 const session = require('express-session')
 const {db} = require('./db')
 const app = express()
@@ -11,8 +10,8 @@ const PORT = 3000
 app.use(morgan('dev'))
 
 // Body parsing middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 // Session middleware
 app.use(session({
